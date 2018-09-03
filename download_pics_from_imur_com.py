e25be8526a3217a634933c4adaa8b41dd5100c9a
hyuk kyu lee <hyukkyulee@alumni.emory.edu>
2018-09-03 19:42:45 -0400

    commit1

--- download_pics_from_imur_com.py
+++ download_pics_from_imur_com.py
@@ -1,4 +1,3 @@
-#-*- coding: utf-8 -*-
 from PIL import Image
 import requests
 from io import BytesIO
@@ -9,8 +8,7 @@ import re
 
 
 
-html = urlopen("https://imgur.com/a/3Woti")
-soup = BeautifulSoup(html, "html.parser")
+
 
 # 폴더 만들기
 def createFolder(directory):
@@ -19,6 +17,7 @@ def createFolder(directory):
             os.makedirs(directory)
     except OSError:
         print('Error: Creating directory. ' + directory)
+
 
 
 # url 경로로 이미지 파일 열기
@@ -31,18 +30,19 @@ def open_and_save_image(url,foldername, output_img):
     img.save('%s/%s.jpg' % (foldername, output_img))
     return
 
-mydivs = soup.findAll("div", {"class": "post-image-container post-image-container--spacer"})
+
+
+# download
+def download_pics_from_Imur(url):
+    html = urlopen("https://imgur.com/a/3Woti")
+    soup = BeautifulSoup(html, "html.parser")
+    mydivs = soup.findAll("div", {"class": "post-image-container post-image-container--spacer"})
+    for index, image in enumerate(mydivs):
+        open_and_save_image(f"http://i.imgur.com/{image['id']}.jpg", "imur_pictures",f"{index}")
+        print(image['id'])
 
 
 
-
-
-
-
-
-for index, image in enumerate(mydivs):
-    open_and_save_image(f"http://i.imgur.com/{image['id']}.jpg", "imur_pictures",f"{index}")
-    print(image['id'])
-
-
-
+if __name__ == "__main__":
+    # example
+    download_pics_from_Imur("https://imgur.com/a/3Woti")
